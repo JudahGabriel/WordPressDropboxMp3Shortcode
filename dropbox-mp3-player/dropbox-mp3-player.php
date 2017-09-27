6d4673a34bde605de4f9c37fbe35f000b20559d5
Judah Gabriel Himango <judahgabriel@gmail.com>
2017-09-27 00:37:23 -0500

    Initial plugin commit

---
+++ dropbox-mp3-player/dropbox-mp3-player.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+Plugin Name: Dropbox MP3 Shortcode
+Plugin URI: https://github.com/JudahGabriel/WordPressDropboxMp3Shortcode
+Description: Provides a [mp3] shortcode.
+Author: Judah Gabriel Himango
+Version: 1.0
+Author URI: http://twitter.com/judahgabriel
+Tags: Search
+License: GPLv3
+*/
+
+// [mp3]
+function mp3_func( $atts ) {
+    $a = shortcode_atts( array(
+        0 => 'http://missing'
+    ), $atts );
+
+	$mp3url = str_replace("www.dropbox", "dl.dropbox", str_replace("?dl=0", "?", $a[0]));
+    return "<audio controls style='width: 100%'><source src='{$mp3url}' type='audio/mpeg'>Your browser does not support the audio element.</audio>";
+}
+add_shortcode( 'mp3', 'mp3_func' );
+
+
+?>
